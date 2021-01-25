471aa4cfcf3f7525bcb6e423844e09ca1a7506bd
arq-gabo <jgabrielguerral@gmail.com>
2021-01-24 23:22:13 -0500

    Custom Types

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
 'use strict'
 
-const { graphql, buildSchema } = require('graphql')
+const { buildSchema } = require('graphql')
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers')
 
 const app = express()
 const port = process.env.PORT || 3000
 
-//definiendo el esquema
+// definiendo el esquema
 
-const schema = buildSchema(`
-    type Query {
-        hello: String
-    }
-`)
-
-// Configurar los resolvers
-const resolvers = {
-    hello: () => {
-        return 'Hola Mundo!!!'
-    },
-}
+const schema = buildSchema(
+    readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
+  );
+  
 
 app.use('/api', graphqlHTTP({
-    schema: schema,
-    rootValue: resolvers,
-    graphiql: true
+  schema: schema,
+  rootValue: resolvers,
+  graphiql: true
 }))
 
 app.listen(port, () => {
-    console.log(`Server on listening at http://localhost:${port}/api`)
+  console.log(`Server on listening at http://localhost:${port}/api`)
 })
