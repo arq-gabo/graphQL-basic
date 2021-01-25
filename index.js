b5f4d60a43b2cfb2bc7f5ec53a1411766e210cc2
arq-gabo <jgabrielguerral@gmail.com>
2021-01-25 14:55:13 -0500

    Mutations e Inputs

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
-'use strict'
+"use strict";
 
-require('dotenv').config()
-const { makeExecutableSchema } = require('graphql-tools')
-const express = require('express')
-const { graphqlHTTP } = require('express-graphql')
-const { readFileSync } = require('fs')
-const { join } = require('path')
-const resolvers = require('./lib/resolvers')
+require("dotenv").config();
+const { makeExecutableSchema } = require("graphql-tools");
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const { readFileSync } = require("fs");
+const { join } = require("path");
+const resolvers = require("./lib/resolvers");
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 // definiendo el esquema
-const typeDefs = readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
+const typeDefs = readFileSync(
+	join(__dirname, "lib", "schema.graphql"),
+	"utf-8"
+);
 
-const schema = makeExecutableSchema({ typeDefs, resolvers })    
-  
-app.use('/api', graphqlHTTP({
-  schema: schema,
-  rootValue: resolvers,
-  graphiql: true
-}))
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+app.use(
+	"/api",
+	graphqlHTTP({
+		schema: schema,
+		rootValue: resolvers,
+		graphiql: true
+	})
+);
 
 app.listen(port, () => {
-  console.log(`Server on listening at http://localhost:${port}/api`)
-})
+	console.log(`Server on listening at http://localhost:${port}/api`);
+});
